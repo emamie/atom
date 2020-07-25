83f71d0b3fb84106704435feb5704b0f0dc6c375
gholami-a <razavi.gholami@gmail.com>
2020-07-25 18:33:52 +0430

    add package redis

--- src/app/Helper/Tools.php
+++ src/app/Helper/Tools.php
@@ -20,3 +20,27 @@ function atomPersianToGregorian($date, $output_format = "yyyy/MM/dd")
 
     return $date->format($output_format);
 }
+
+
+function atomIsNationalId($code) {
+    if (!preg_match('/^[0-9]{10}$/', $code)) {
+        return false;
+    }
+
+    for ($i = 0; $i < 10; $i++) {
+        if (preg_match('/^' . $i . '{10}$/', $code)) {
+            return false;
+        }
+    }
+
+    for ($i = 0, $sum = 0; $i < 9; $i++) {
+        $sum += ((10 - $i) * intval(substr($code, $i, 1)));
+    }
+    $ret = $sum % 11;
+    $parity = intval(substr($code, 9, 1));
+    if (($ret < 2 && $ret == $parity) || ($ret >= 2 && $ret == 11 - $parity)) {
+        return true;
+    }
+
+    return false;
+}
